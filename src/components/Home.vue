c35513b5b265e3951837af2ee84b12ac0a2a63e1
Ironi Jr. Medina <ironimedina@gmail.com>
2017-11-13 22:41:10 -0200

    remove unnecessary stuff

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -86,32 +86,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-#home {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 5px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
