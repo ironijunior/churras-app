bba03f6a574835b939936271e5dc3feb7b716f0a
Ironi Jr. Medina <ironimedina@gmail.com>
2017-11-13 22:38:23 -0200

    Crazy modifications

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,63 @@
 <template>
-  <div id="home">
-    <img src="../assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <hr></hr>
-    <br />
-    <h2>Next Events</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">#1 Riachuleto - Done</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">#2 Riachuleto - Coming</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Party Hard</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">LA la LA la</a></li>
-    </ul>
-    <br />
-    <ul>
-      <li><router-link :to="'/login'">Login</router-link></li>
-      <li><router-link :to="'/signUp'">Sign Up</router-link></li>
-    </ul>
+    <div style="margin-top: 10px">
+      <md-card>
+        <md-card-media>
+          <img src="https://abrilvip.files.wordpress.com/2016/09/churras5.jpg?quality=70&strip=info&w=620" alt="People">
+        </md-card-media>
+
+        <md-card-header>
+          <div class="md-title">#2 Riachuleto</div>
+          <div class="md-subhead">Segunda edição do Riachuleto, o churras da Riachuelo!</div>
+        </md-card-header>
+      </md-card>
+
+      <br />
+      <md-card class="md-accent">
+        <md-card-header>
+          <div class="md-title">Confirme sua presença!</div>
+        </md-card-header>
+        <md-card-content>
+          <p><md-checkbox
+            id="my-test2"
+            name="my-test2"
+            v-model="checkbox"
+            class="md-primary">
+            Vai beber? (bebida alcoólica)
+          </md-checkbox></p>
+          <p><md-checkbox
+            id="my-test1"
+            name="my-test1"
+            v-model="checkbox"
+            class="md-primary">
+            Vai comer?
+          </md-checkbox></p>
+          <md-button class="md-raised md-primary">Participar</md-button>
+        </md-card-content>
+      </md-card>
+
+      <br />
+      <md-card>
+        <md-card-header>
+          <div class="md-title">Participantes</div>
+        </md-card-header>
+          <md-list class="custom-list md-double-line">
+            <md-list-item v-for="person in people">
+              <md-avatar>
+                <img src="https://placeimg.com/40/40/people/1" alt="People">
+              </md-avatar>
+
+              <div class="md-list-text-container">
+                <span>{{ person.name }}</span>
+                <span>{{ person.floor }}°</span>
+              </div>
+
+              <md-button class="md-icon-button md-list-action">
+                <md-icon class="md-primary">phone</md-icon>
+              </md-button>
+              <md-divider class="md-inset"></md-divider>
+            </md-list-item>
+        </md-list>
+      </md-card>
   </div>
 </template>
 
@@ -23,6 +65,22 @@
 export default {
   data () {
     return {
+      people: [{
+        name: 'Ironi',
+        floor: 2
+      },{
+        name: 'Italo',
+        floor: 2
+      },{
+        name: 'Zotto',
+        floor: 4
+      },{
+        name: 'Henrique',
+        floor: 2
+      },{
+        name: 'Cesar',
+        floor: 1
+      }],
       msg: 'Welcome to ChurrasCumbras!'
     }
   }
